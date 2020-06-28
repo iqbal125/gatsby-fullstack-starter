91a029c9d1b74986017092805d8ce3e43844160f
iqbal125 <iqbal125@yahoo.com>
2020-06-28 16:57:44 -0700

    added sidebar animation

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,6 +3,7 @@ import styles from './sidebar.module.css';
 import GeneralContext from '../../utils/general_context';
 import AuthContext from '../../utils/auth_context';
 import { navigate } from 'gatsby';
+import { MdAccountCircle } from 'react-icons/md';
 
 const SideBar = props => {
   const contextGeneral = useContext(GeneralContext);
@@ -12,14 +13,23 @@ const SideBar = props => {
     navigate('/');
     context.firebase.auth().signOut();
     setTimeout(() => context.LogOut(), 200);
+    contextGeneral.closeSideDrawer();
   };
 
   const goToProfile = () => {
     navigate('/app/profile');
+    contextGeneral.closeSideDrawer();
   };
 
   return (
     <div className={styles.side_drawer}>
+      <div className={styles.header_photo_wrap_desk}>
+        {context.authState.user.photo ? (
+          <img src={context.authState.user.photo} className={styles.header_photo} alt="Not Found" />
+        ) : (
+          <MdAccountCircle className={styles.header_photo} />
+        )}
+      </div>
       <div className={styles.side_items} onClick={goToProfile}>
         Profile
       </div>
