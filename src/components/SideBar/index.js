81d032692518b32cb029598044f92aada5f6d930
iqbal125 <iqbal125@yahoo.com>
2020-06-28 17:10:55 -0700

    outside click on sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
 import styles from './sidebar.module.css';
 import GeneralContext from '../../utils/general_context';
 import AuthContext from '../../utils/auth_context';
@@ -8,6 +8,20 @@ import { MdAccountCircle } from 'react-icons/md';
 const SideBar = props => {
   const contextGeneral = useContext(GeneralContext);
   const context = useContext(AuthContext);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      contextGeneral.closeSideDrawer();
+    }
+  };
 
   const logOut = () => {
     navigate('/');
@@ -22,7 +36,7 @@ const SideBar = props => {
   };
 
   return (
-    <div className={styles.side_drawer}>
+    <div ref={ref} className={styles.side_drawer}>
       <div className={styles.header_photo_wrap_desk}>
         {context.authState.user.photo ? (
           <img src={context.authState.user.photo} className={styles.header_photo} alt="Not Found" />
