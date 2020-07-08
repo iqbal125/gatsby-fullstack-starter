2839d7235f80f4fba4dde8106cf0c841dbf32618
iqbal125 <iqbal125@yahoo.com>
2020-07-07 18:36:52 -0700

    fix private route

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,24 +1,19 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
 import { navigate } from 'gatsby';
 import Profile from '../Profile';
-import AuthContext from '../../utils/auth_context';
+
 import Auth from '../Authentication/auth';
 
 const Routes = () => {
-  const context = useContext(AuthContext);
   //check token expires time on private routes
   const isTokenValid = () => {
     let expiresAt = JSON.parse(localStorage.getItem('expiresIn'));
     return new Date().getTime() < expiresAt;
   };
 
-  const validateToken = isTokenValid();
-
   const PrivateRoute = ({ component: Component, location, ...rest }) => {
-    if (!validateToken) {
-      context.LogOut();
-      context.firebase.auth().signOut();
+    if (!isTokenValid()) {
       navigate('/app/login');
       return null;
     } else {
