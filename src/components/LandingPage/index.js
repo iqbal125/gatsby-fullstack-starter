3db86ffa507fdbbc4263d79df1295053d22e1693
iqbal125 <iqbal125@yahoo.com>
2020-07-15 18:11:47 -0700

    added landing page animations

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { navigate } from 'gatsby';
 import SocialBar from '../SocialBar';
 
@@ -16,20 +16,52 @@ import pic3 from '../../../static/uploads/healthcare.jpg';
 import styles from './home.module.css';
 
 const Home = () => {
-  const toContactPage = () => {
-    navigate('/contact');
+  const intersectTargetFeat1 = useRef(null);
+  const intersectTargetFeat2 = useRef(null);
+  const [feat1Intersect, setFeat1] = useState(false);
+  const [feat2Intersect, setFeat2] = useState(false);
+
+  const toLoginPage = () => {
+    navigate('/app/login');
   };
 
   const toAboutPage = () => {
     navigate('/about');
   };
 
+  useEffect(() => {
+    const opts = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
+    const callback = list => {
+      list.forEach(entry => {
+        let isFeat1 = entry.target.className.includes('feature1');
+        let isFeat2 = entry.target.className.includes('feature2');
+
+        if (entry.isIntersecting && isFeat1) {
+          console.log('feat1');
+          setFeat1(true);
+        }
+        if (entry.isIntersecting && isFeat2) {
+          console.log(entry);
+          console.log('feat2');
+          setFeat2(true);
+        }
+      });
+    };
+    const observerScroll = new IntersectionObserver(callback, opts);
+
+    observerScroll.observe(intersectTargetFeat1.current);
+    observerScroll.observe(intersectTargetFeat2.current);
+  }, []);
+
   return (
     <div>
       <div className={styles.main}>
         <div className={styles.landing_hero}>
           <h1 className={styles.value_prop}>Main Value Prop</h1>
-
           <img className={styles.hero_illustration} src={image3} alt="" />
         </div>
         <div className={styles.wave_bottom_hero}>
@@ -46,19 +78,40 @@ const Home = () => {
               Find Out More
             </button>
           </div>
-          <img className={styles.feature1_img} src={image1} alt="" />
+          <div className={styles.img_container}>
+            <img
+              ref={intersectTargetFeat1}
+              className={feat1Intersect ? styles.feature1_img : styles.feature1_img_none}
+              src={image1}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={styles.feature2}>
-          <img className={styles.feature2_img} src={image5} alt="" />
+        <div ref={intersectTargetFeat2} className={styles.feature2}>
+          <div className={styles.img_container}>
+            <img
+              className={feat2Intersect ? styles.feature2_img : styles.feature2_img_none}
+              src={image5}
+              alt=""
+            />
+          </div>
           <div className={styles.feature2_text}>
             <h2>Feature #2</h2>
             <p>Explanation of Feature</p>
           </div>
+          <img
+            ref={intersectTargetFeat2}
+            className={
+              feat2Intersect ? styles.feature2_img_mobile : styles.feature2_img_mobile_none
+            }
+            src={image5}
+            alt=""
+          />
         </div>
         <div className={styles.bottom_cta_section}>
           <div className={styles.bottom_cta_text}>
             <h3>Get in touch to see how we can help you on your next project</h3>
-            <button className={styles.get_started_button} onClick={toContactPage}>
+            <button className={styles.get_started_button} onClick={toLoginPage}>
               Get Started Now
             </button>
           </div>
