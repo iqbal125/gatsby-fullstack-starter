303413ccd9b2f5b2c66161ca0e879b18a428bd08
iqbal125 <iqbal125@yahoo.com>
2020-08-08 19:22:31 -0700

    remove full profile card

--- src/components/ProfileCardSection/index.js
+++ src/components/ProfileCardSection/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ProfileCard from '../ProfileCard';
-import ProfileCardFull from '../ProfileCardFull';
 import styles from './profilecardsection.module.css';
-import { profiles, profile1 } from './profileinfo.js';
-
-
+import { profiles } from './profileinfo.js';
 
 const ProfileCardSection = () => {
   return (
@@ -13,9 +10,6 @@ const ProfileCardSection = () => {
         <h1>Example Mission Statement </h1>
         <h3>Main values of Business</h3>
         <p>Detailed explanation</p>
-      </div>
-      <div className={styles.top_card}>
-        <ProfileCardFull profile={profile1} />
       </div>
       <div className={styles.card_section}>
         {profiles.map(profile => (
--- src/components/ProfileCardSection/profileinfo.js
+++ src/components/ProfileCardSection/profileinfo.js
@@ -1,16 +1,15 @@
 import image1 from '../../../static/profiles/male-default-placeholder-avatar-profile-260nw-582509551.webp';
 
-export const profile1 = {
-  id: 1,
-  image: image1,
-  name: 'John Smith',
-  title: 'Founder and CEO',
-  description: ``
-};
-
 export const profiles = [
   {
     id: 1,
+    image: image1,
+    name: 'John Smith',
+    title: 'Founder and CEO',
+    description: ``
+  },
+  {
+    id: 2,
     image: image1,
     name: 'John Doe 1',
     title: 'VP of Sales',
@@ -18,7 +17,7 @@ export const profiles = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    id: 2,
+    id: 3,
     image: image1,
     name: 'John Doe 2',
     title: 'Product Manager',
@@ -26,7 +25,7 @@ export const profiles = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    id: 3,
+    id: 4,
     image: image1,
     name: 'John Doe 3',
     title: 'Cloud Specialist.',
@@ -34,7 +33,7 @@ export const profiles = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    id: 4,
+    id: 5,
     image: image1,
     name: 'John Doe 4',
     title: 'Manager Executive',
@@ -42,7 +41,7 @@ export const profiles = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    id: 5,
+    id: 6,
     image: image1,
     name: 'John Doe 5',
     title: 'Full Stack Developer',
